b4655ace62aee978e7ca902a6e7922f0167a78e9
Jaaarl <jarl13.work@gmail.com>
2024-09-10 16:40:28 +0800

    combining 2 arrays into 1

---
+++ array_and_hashes/act_14.rb
@@ -0,0 +1,5 @@
+color_1 = ["red", "green", "blue"]
+color_2 = ["yellow", "purple", "pink"]
+combine_color = color_1 + color_2
+puts "combining this 2 array into 1 #{color_1} #{color_2}"
+print combine_color
