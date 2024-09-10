018f8bd52cbc77436ea8250f57313646a5ba6bff
Jaaarl <jarl13.work@gmail.com>
2024-09-10 16:34:50 +0800

    replacing 3rd index with tiger

---
+++ array_and_hashes/act_13.rb
@@ -0,0 +1,4 @@
+animals = ["Lion", "cat", "dog", "parrot", "cockroach"]
+print "This is array of animals before replacing 3rd index with tiger \n#{animals}\nAnd this is After\n"
+animals[2] = "tiger"
+print animals
