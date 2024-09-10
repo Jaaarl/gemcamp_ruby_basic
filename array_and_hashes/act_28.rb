ab375f548cde5af2c7bfd4d2790f5f1446ac416f
Jaaarl <jarl13.work@gmail.com>
2024-09-10 19:24:57 +0800

    print fav movie in hash

---
+++ array_and_hashes/act_28.rb
@@ -0,0 +1,3 @@
+# Create a hash to store information about your favorite movie: title, director, year, and rating. Print the entire hash.
+movie = {title: "Avengers", director: "Joe Ruso",year: "2012",rating: 4.7}
+print "my favorite movie in hash is #{movie}"
