d1ded9906927073c561f93b83f7cb71a60fc7a17
Jaaarl <jarl13.work@gmail.com>
2024-09-10 18:52:43 +0800

    change the year value to 2020

---
+++ array_and_hashes/act_25.rb
@@ -0,0 +1,4 @@
+car = { maker: "Toyota", model: "Corolla", year: 2015 }
+puts "this is car before updating the year \n#{car}\n"
+car[:year] = 2020
+puts "this is car before after the year \n#{car}\n"
