c5995556ce11550d0a736ec65854e6df5d28f83c
Jaaarl <jarl13.work@gmail.com>
2024-09-11 09:06:38 +0800

    print 2nd product from store 2

---
+++ array_and_hashes/act_36.rb
@@ -0,0 +1,5 @@
+stores = {
+  store_1: { products: ["apples", "oranges"]},
+  store_2: { products: ["bananas", "grapes"]}
+}
+print "the second product of store 2 is #{stores[:store_2][:products][1]}"
