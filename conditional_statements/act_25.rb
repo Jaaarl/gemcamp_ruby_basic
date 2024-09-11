db3a958612bb4181adda0b1166d869cd481cd6f1
Jaaarl <jarl13.work@gmail.com>
2024-09-11 16:58:42 +0800

    checks num if divisible by 3,5,3&5 or not divisible

---
+++ conditional_statements/act_25.rb
@@ -0,0 +1,16 @@
+# Write a program that asks for a number and checks if it is divisible by 3 and/or 5.
+# If divisible by both, print The number is divisible by both 3 and 5.
+# If only divisible by 3, print The number is divisible by 3.
+# If only divisible by 5, print The number is divisible by 5.
+# Otherwise, print the number is not divisible by 3 or 5.
+puts "enter a number"
+number = gets.chomp.to_i
+if number% 3 == 0 && number% 5 == 0
+  puts "The number is divisible by both 3 and 5."
+elsif number% 3 == 0
+  puts "The number is divisible by 3"
+elsif number% 5 == 0
+  puts "The number is divisible by 5"
+else
+  puts "The number is not divisible by 3 or 5"
+end
