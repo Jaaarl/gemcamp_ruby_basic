8b927b6515faa266541cb234617eeaa1ad653100
Jaaarl <jarl13.work@gmail.com>
2024-09-11 16:17:02 +0800

    tells if both word are identical or not

---
+++ conditional_statements/act_17.rb
@@ -0,0 +1,9 @@
+puts "enter a word"
+word_1 = gets.chomp
+puts "enter a word"
+word_2 = gets.chomp
+if word_1 === word_2
+  print "The words are identical"
+else
+  print "The words are different"
+end
