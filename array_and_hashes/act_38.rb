70e3cd7941415beaa967de8e6c10774ed229cf36
Jaaarl <jarl13.work@gmail.com>
2024-09-11 09:20:12 +0800

    printing my birthday using 2d array

---
+++ array_and_hashes/act_38.rb
@@ -0,0 +1,7 @@
+number = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  ['-', 0]
+]
+print "my birthday is #{number[0][0]}#{number[2][2]}#{number[2][2]}#{number[2][2]}#{number[3][0]}#{number[0][0]}#{number[0][1]}#{number[3][0]}#{number[3][2]}#{number[2][1]}"
