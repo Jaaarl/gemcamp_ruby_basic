0030d3aa8baeacbf11416305af1aa3ef927e205f
Jaaarl <jarl13.work@gmail.com>
2024-09-11 08:58:42 +0800

    print the player 2 in team b

---
+++ array_and_hashes/act_33.rb
@@ -0,0 +1,6 @@
+teams =
+  {
+    team_a: {player_1: "John", player_2: "Paul"},
+    team_b: {player_1: "Sarah", player_2: "Lucy"}
+  }
+print "the player 2 in team b is #{teams[:team_b][:player_2]}"
