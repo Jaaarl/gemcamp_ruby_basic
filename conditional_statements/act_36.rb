ee014d27bcca84273779fd83b26648bfc5ed3adb
Jaaarl <jarl13.work@gmail.com>
2024-09-12 09:07:18 +0800

    capitilize all the word

--- conditional_statements/act_36.rb
+++ conditional_statements/act_36.rb
@@ -1,4 +1,3 @@
 names = ["jose mari" ,"jerome" ,"precious" ,"andeng" ,"leronie"]
 puts "this is the array before capitalize \n#{names}\nthis is after capitalize"
 p names.map { |name|name.capitalize  }
-# p num.map{|item| item.to_i}
